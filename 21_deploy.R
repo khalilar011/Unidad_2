d6350c395ad4b0ae7404d9288a6d29f0bbe3e6c9
Khalil <khalilarnu011@gmail.com>
2025-11-04 14:32:17 -0500

    Nueva app (generador de muestra)

--- 21_deploy.R
+++ 21_deploy.R
@@ -5,7 +5,7 @@ rsconnect::setAccountInfo(name='khalilarabn11',
                           token='C9E48356AD2FED854035351314FDB7EE',
                           secret='I39En5tIHiUWJxFsiZ8R+ltrJuOGUUZHtmXqQ/26')
 
-rsconnect::deployApp(appDir = "C:/Users/khali/OneDrive/Desktop/Universidad/Estadistica",
+rsconnect::deployApp(appDir = "C://Users/khali/OneDrive/Desktop/Universidad/Estadistica/Unidad_2/Shiny_0",
                      appName = "21_myapp")
 
 
---
+++ 22_shiny/R/stats.R
@@ -0,0 +1,20 @@
+generar_poblacion <- function(seed, n, p) {
+  set.seed(seed)
+  rbinom(n, 1, p)
+}
+
+# x <- generar_poblacion(123, 1000, 0.3)
+
+tomar_muestra <- function(poblacion, n_muestra) {
+  sample(poblacion, n_muestra, replace = FALSE)
+}
+
+# tomar_muestra(c(1, 0, 0, 1, 0, 1, 0), 2)
+# y <- tomar_muestra(x, 100)
+
+calcular_proporcion <- function(muestra) {
+  sum(muestra) / length(muestra)
+}
+
+# calcular_proporcion(y)
+
